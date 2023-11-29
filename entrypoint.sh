24c189a2884547c05ae0673c893edd7a95d1f0a9
Jose <jose@minehub.com>
2023-11-29 09:20:52 +0000

    It now accepts multiple components in one go

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,10 @@ git config user.name "github-actions"
 git config user.email "github-actions@users.noreply.github.com"
 
 getOrganizations() {
+  if $INPUT_ORGANIZATIONS; then
+    echo "${INPUT_ORGANIZATIONS}"
+    return
+  fi
   set -e
 
   local listOfOrganizations
@@ -19,25 +23,39 @@ getOrganizations() {
 }
 
 organizations=$(getOrganizations)
-
+component_array=(${INPUT_COMPONENT_NAME})
 for organization in $organizations; do
   echo $organization
+  component_does_not_exist=true
 
-  if [ ! -d "${organization}/applications/${INPUT_COMPONENT_NAME}" ]; then
-    echo "========== ${organization} ${INPUT_COMPONENT_NAME} does not exist =========="
+  for component in "${component_array[@]}"; do
+      if [ ! -d "${organization}/applications/${component}" ]; then
+        echo "========== ${organization} ${component} does not exist =========="
+      else
+          component_does_not_exist=false
+      fi
+  done
+
+  if [ "${component_does_not_exist}" = true ]; then
+    echo "========== ${organization} does not have any component of ${INPUT_COMPONENT_NAME} =========="
     continue
   fi
 
-  pushd "${organization}/applications/${INPUT_COMPONENT_NAME}"
 
-  yq --yaml-output \
-     --arg version "${INPUT_VERSION}" \
-      '(.spec.source.helm.parameters[] | select (.name == "image.tag") | .value) = $version'\
-      "${INPUT_COMPONENT_NAME}.yaml" | sponge "${INPUT_COMPONENT_NAME}.yaml"
+  for component in "${component_array[@]}"; do
 
-  echo "========== Updated ${organization} ${INPUT_COMPONENT_NAME} YAML =========="
-  cat ${INPUT_COMPONENT_NAME}.yaml
-  echo "========== Updated YAML =========="
+    if [ ! -d "${organization}/applications/${component}" ]; then
+    pushd "${organization}/applications/${component}"
 
-  popd
+    yq --yaml-output \
+       --arg version "${INPUT_VERSION}" \
+        '(.spec.source.helm.parameters[] | select (.name == "image.tag") | .value) = $version'\
+        "${component}.yaml" | sponge "${component}.yaml"
+
+    echo "========== Updated ${organization} ${component} YAML =========="
+    cat ${component}.yaml
+    echo "========== Updated YAML =========="
+
+    popd
+  done
 done
